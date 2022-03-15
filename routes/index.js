152c9abbe32661e7aafc5c67ab14ab0594d64cda
DJAwaits <padjcreeper@gmail.com>
2022-03-15 08:55:18 -0700

    second commit, slide 76

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
+const {Comment, Reply} = require('../models');
 var express = require('express');
 var router = express.Router();
 const articleController = require('../controllers/articleController');
-
+const commentController = require('../controllers/commentController');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect('/article');
@@ -10,5 +11,12 @@ router.get('/article/add', articleController.renderAddForm);
 router.post('/article/add', articleController.addArticle);
 
 router.get('/article/:articleId', articleController.displayArticle);
-router.get('/article/', articleController.displayArticle);
+router.get('/article/', articleController.viewAll);
+router.get('/article/:articleId/edit', articleController.renderEditForm);
+router.post('/article/:articleId/edit', articleController.updateArticle);
+router.get('/article/:articleId/delete', articleController.deleteArticle);
+
+
+router.post('/comment/:commentId/reply/create', commentController.addReply);
+router.post('/article/:articleId/comment/create', commentController.createComment);
 module.exports = router;
