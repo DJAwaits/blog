54a5f43b334e86b33b6c8f2ffb344b8337ed9ef0
DJAwaits <padjcreeper@gmail.com>
2022-03-29 08:19:08 -0700

    fifth commit, errors encountered on slide 41

--- models/user.js
+++ models/user.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.belongsTo(models.Role, {
+        as: 'role'
+      })
     }
   };
   User.init({
